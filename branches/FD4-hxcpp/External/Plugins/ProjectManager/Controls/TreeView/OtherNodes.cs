6f575b837a6af1985a6c8f4c75d7f2f56fb1fe15
damjan.cvetko@gmail.com <damjan.cvetko@gmail.com@2354be2f-a62e-0410-afb7-89360a396392>
2013-08-09 19:54:49 +0000

    Preparing branch for hxcpp debugger development

---
+++ branches/FD4-hxcpp/External/Plugins/ProjectManager/Controls/TreeView/OtherNodes.cs
@@ -0,0 +1,130 @@
+using System;
+using System.IO;
+using System.Drawing;
+using System.Collections;
+using System.Diagnostics;
+using ProjectManager.Projects;
+using System.Text.RegularExpressions;
+
+namespace ProjectManager.Controls.TreeView
+{
+	public class ProjectNode : WatcherNode
+	{
+		public ProjectNode(Project project) : base(project.Directory)
+		{
+			isDraggable = false;
+			isRenamable = false;
+		}
+
+		public override void Refresh(bool recursive)
+		{
+			base.Refresh(recursive);
+            Text = Project.Name + " (" + Project.Language.ToUpper() + ")";
+			ImageIndex = Icons.Project.Index;
+			SelectedImageIndex = ImageIndex;
+			NodeFont = new System.Drawing.Font(PluginCore.PluginBase.Settings.DefaultFont, FontStyle.Bold);
+            Expand();
+
+            NotifyRefresh();
+		}
+	}
+
+	public class ClasspathNode : WatcherNode
+	{
+        public string classpath;
+
+		public ClasspathNode(Project project, string classpath, string text) : base(classpath)
+		{
+			isDraggable = false;
+			isRenamable = false;
+
+            this.classpath = classpath;
+
+            // shorten text
+            string[] excludes = PluginMain.Settings.FilteredDirectoryNames;
+            char sep = Path.DirectorySeparatorChar;
+            string[] parts = text.Split(sep);
+            string label = "";
+            Regex reVersion = new Regex("^[0-9]+[.,-][0-9]+");
+            string version = null;
+
+            if (parts.Length > 0)
+            {
+                for (int i = parts.Length - 1; i > 0; --i)
+                {
+                    String part = parts[i] as String;
+                    if (part != "" && part != "." && part != ".." && Array.IndexOf(excludes, part.ToLower()) == -1)
+                    {
+                        if (Char.IsDigit(part[0]) && reVersion.IsMatch(part)) version = part;
+                        else
+                        {
+                            label = part;
+                            break;
+                        }
+                    }
+                }
+                if (label == "")
+                {
+                    label = parts[parts.Length - 1];
+                }
+            }
+            if (version != null) label += " (" + version + ")";
+            Text = label;
+            ToolTipText = classpath;
+		}
+
+		public override void Refresh(bool recursive)
+		{
+
+			base.Refresh(recursive);
+
+            base.isInvalid = !Directory.Exists(BackingPath);
+
+            if (!isInvalid)
+            {
+                ImageIndex = Icons.Classpath.Index;
+            }
+            else
+            {
+                ImageIndex = Icons.ClasspathError.Index;
+            }
+
+            SelectedImageIndex = ImageIndex;
+
+            NotifyRefresh();
+		}
+	}
+
+    public class ProjectClasspathNode : ClasspathNode
+    {
+        public ProjectClasspathNode(Project project, string classpath, string text) : base(project, classpath, text)
+        {
+            if (text != Text)
+            {
+                ToolTipText = text;
+            }
+            else
+            {
+                ToolTipText = "";
+            }
+        }
+
+        public override void Refresh(bool recursive)
+        {
+            base.Refresh(recursive);
+
+            if (!IsInvalid)
+            {
+                ImageIndex = Icons.ProjectClasspath.Index;
+            }
+            else
+            {
+                ImageIndex = Icons.ProjectClasspathError.Index;
+            }
+
+            SelectedImageIndex = ImageIndex;
+
+            NotifyRefresh();
+        }
+    }
+}
